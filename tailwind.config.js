24a695e2650c5cd1233f2a905a3e2661f5743e0b
Yamile Ibarra <51417561+yamilic008@users.noreply.github.com>
2020-11-01 22:30:30 -0600

    Primer Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#f3c614',
+        'secundary': '#353535',
+      },
+      height:{
+        '80': '20rem',
+        '100': '25rem',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
